9724d97fec4908af3c4e00858977b567d632b82c
Pablo  Aguilar <135460549+pablo-972@users.noreply.github.com>
2024-10-21 21:14:57 +0200

    Some scripts

---
+++ Wi-Fi Spam.py
@@ -0,0 +1,60 @@
+import threading
+from scapy.all import *
+import random
+
+class colors:
+    YELLOW = "\033[93m"  
+    BLUE = "\033[94m"    
+    END = "\033[0m"
+
+print(colors.YELLOW + "==========================================================\n" + colors.END)
+print(colors.BLUE + 
+" __        ___       _____ _   ____                         \n" +
+" \\ \\      / (_)     |  ___(_) / ___| _ __   __ _ _ __ ___  \n" +
+"  \\ \\ /\\ / /| |_____| |_  | | \\___ \\| '_ \\ / _` | '_ ` _ \\ \n" +
+"   \\ V  V / | |_____|  _| | |  ___) | |_) | (_| | | | | | |\n" +
+"    \\_/\\_/  |_|     |_|   |_| |____/| .__/ \\__,_|_| |_| |_|\n" +
+"                                   |_|                       " + colors.END)
+print(colors.YELLOW + "\n==========================================================\n" + colors.END)
+
+print("BY DYGRAL\n")
+
+time.sleep(5)
+
+# This generates the random name to the network
+def generar_codigo(length=8):
+    caracteres = string.ascii_letters + string.digits  
+    codigo = ''.join(random.choices(caracteres, k=length))
+    return codigo
+
+
+def create_wifi():
+	interface = "<Change it whith your interface in monitor mode>"
+	sender = RandMAC() 
+	access_point_name =  generar_codigo() 
+
+	dot11 = Dot11(type=0, subtype=8, 
+				addr1="ff:ff:ff:ff:ff:ff", 
+				addr2=sender, addr3=sender) 
+	beacon = Dot11Beacon() 
+
+	e_SSID = Dot11Elt(ID="SSID", info=access_point_name, 
+					len=len(access_point_name)) 
+
+	frame = RadioTap()/dot11/beacon/e_SSID 
+
+	sendp(frame, inter=0.1, iface=interface, loop=1) 
+
+
+threads = []
+
+# Change the range to your choice
+for i in range(200):
+		thread = threading.Thread(target=create_wifi)
+		threads.append(thread)
+		thread.start()
+	
+for thread in threads:
+	thread.join()
+
+
---
+++ key_spam.py
@@ -0,0 +1,61 @@
+import keyboard
+import threading
+import time
+import sys
+import pyautogui
+
+running = True
+
+class colors:
+    RED = "\033[91m"  
+    END = "\033[0m"
+
+print(colors.RED+"=======================================================================================================\n"+colors.END)
+
+print(" /\\ \\/ /    /\\  ___\\   /\\ \\_\\ \\      /\\  ___\\   /\\  == \\ /\\  __ \\   /\\ \"-./  \\ ")
+print(" \\ \\  _\"-.  \\ \\  __\\   \\ \\____ \\     \\ \\___  \\  \\ \\  _-/ \\ \\  __ \\  \\ \\ \\-./\\ \\ ")
+print("  \\ \\_\\ \\_\\  \\ \\_____\\  \\/\\_____\\     \\/\\_____\\  \\ \\_\\    \\ \\_\\ \\_\\  \\ \\_\\ \\ \\_\\")
+print("   \\/_/\\/_/   \\/_____/   \\/_____/      \\/_____/   \\/_/     \\/_/\\/_/   \\/_/  \\/_/ "+colors.END)
+
+print(colors.RED+"\n=======================================================================================================\n"+colors.END)
+
+print("BY DYGRAL\n")
+
+# This function send "@"
+def send_alt_2():
+    pyautogui.hotkey('altright', '2')
+
+# This function is a complement to send_alt_2
+def send_tab():
+    pyautogui.hotkey('tab')
+
+# This function send whatever you have in your clipboard
+def send_ctrl_v():
+    pyautogui.hotkey('ctrl', 'v')
+
+def send_e():
+    keyboard.press("e")
+    time.sleep(0.01)  
+    keyboard.release("e")
+
+# This function send the letter "e" non-stop
+def send():
+    global running
+    while running:
+        send_e()  
+        keyboard.press("enter")
+        time.sleep(0.01)  
+        keyboard.release("enter")
+
+
+
+time.sleep(5)
+keyboard_thread = threading.Thread(target=send)
+keyboard_thread.start()
+
+# Press "s" repeatedly to stop the thread
+while True:
+    if keyboard.is_pressed("s"):
+        running = False
+        keyboard_thread.join()
+        sys.exit(1)
